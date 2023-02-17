391f4b70beb23a29f106166e7b2ccba19b084aaa
TLaloo <Tricia_Laloo@quadra.ca>
2023-02-17 14:06:19 -0500

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by Tricia Laloo and is
-        <a href="https://github.com/TLaloo/react-weather-app" target="_blank">
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          This project was coded by Tricia Laloo and is{" "}
+          <a href="https://github.com/TLaloo/react-weather-app" target="_blank">
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
